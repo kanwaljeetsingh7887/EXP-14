b291d891f16733c9100d53ee12e6b36be6c56568
Kanwaljeet Singh <97405821+kanwaljeetsingh7887@users.noreply.github.com>
2024-09-11 09:29:19 +0530

    Add files via upload

---
+++ Code14C.cpp
@@ -0,0 +1,35 @@
+// NAME - Kanwaljeet singh
+// PRN - 23070123124
+// EXPERIMENT - 14(D)
+
+// Program to show Hiererchical Inheritance.                    
+
+#include <iostream>
+using namespace std;
+
+// base class
+class Flower {
+public:
+    Flower() { cout << "This is a Flower. \n"; }
+};
+
+// first sub class
+class Lotus : public Flower {
+public:
+    Lotus() { cout << "This flower is a lotus. \n"; }
+};
+
+// second sub class
+class Marigold : public Flower {
+public:
+    Marigold() { cout << "Thisflower is a marigold. \n"; }
+};
+
+// main function
+int main()
+{
+    // Creating object of sub class will invoke the constructor of base class.
+    Lotus obj1;
+    Marigold obj2;
+    return 0;
+}
